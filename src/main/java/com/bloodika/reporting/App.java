993a7460da0e84dd476850b463330d1615aeb15b
bloodika <bloodika@gmail.com>
2023-01-08 14:08:16 +0100

    SpringBootApp

--- src/main/java/com/bloodika/reporting/App.java
+++ src/main/java/com/bloodika/reporting/App.java
@@ -1,7 +1,11 @@
 package com.bloodika.reporting;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App{
     public static void main(final String[] args) {
-        System.out.println("hello");
+        SpringApplication.run(App.class, args);
     }  
 }
